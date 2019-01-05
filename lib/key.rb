018c2bd86f5869f1085fc7f60063e371e38a93dd
earl-stephens <stephensearl@hotmail.com>
2019-01-05 12:06:16 -0700

    started on testing random number for 5 digits, but had to add test and method for using a test random number

--- lib/key.rb
+++ lib/key.rb
@@ -1,5 +1,6 @@
 class Key
-  attr_reader :random_number
+  attr_reader :random_number,
+              :random_number_array
 
   def initialize
     @random_number = 0
@@ -9,6 +10,15 @@ class Key
     generator = Random.new
     @random_number = generator.rand(100000)
     @random_number.to_i
+  end
+
+  def convert_random_number_to_array
+    @random_number_array = @random_number.digits
+    @random_number_array
+  end
+
+  def test_random_number(number)
+    @random_number = number
   end
 
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -23,4 +23,31 @@ class KeyTest < Minitest::Test
     assert_equal Integer, key.random_number.class
   end
 
+  def test_convert_random_number_to_array
+    key = Key.new
+
+    key.convert_random_number_to_array
+
+    assert_equal Array, key.random_number_array.class
+  end
+
+  def test_to_use_a_test_random_number_method
+    key = Key.new
+
+    key.test_random_number(123)
+
+    assert_equal 123, key.random_number
+
+    key.convert_random_number_to_array
+    assert_equal [3, 2, 1], key.random_number_array
+  end
+
+  def test_random_number_has_5_digits
+    key = Key.new
+
+    key.convert_random_number_to_array = ["1", "2", "3"]
+
+    assert_equal false, key.test_random_number_array_for_5_digits
+  end
+
 end
