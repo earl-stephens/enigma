edaa97060870a6cd0a938d6b8987624515cbf2fc
earl-stephens <stephensearl@hotmail.com>
2019-01-06 13:52:26 -0700

    added test_it_can_use_a_setup_method and setup methods

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,2 +1,19 @@
 class Enigma
+  attr_reader :key,
+              :offset,
+              :shift,
+              :hash
+
+  def setup
+    @key = Key.new
+    @offset = Offset.new
+    @shift = Shift.new
+    @hash = Hash.new
+  end
+
+  def encrypt(message, user_key, user_date)
+    setup
+
+  end
+
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -11,4 +11,27 @@ class EnigmaTest < Minitest::Test
 
     assert_instance_of Enigma, enigma
   end
+
+  def test_it_can_use_a_setup_method
+    enigma = Enigma.new
+    enigma.setup
+
+    assert_instance_of Key, enigma.key
+    assert_instance_of Offset, enigma.offset
+    assert_instance_of Shift, enigma.shift
+    assert_instance_of Hash, enigma.hash
+  end
+
+  def test_it_can_encrypt_a_message
+    skip
+    enigma = Enigma.new
+
+    expected =({
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+      })
+    assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
+  end
+
 end
