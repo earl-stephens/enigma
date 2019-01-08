1f952300528835180b39c5b199a806f74826e125
earl-stephens <stephensearl@hotmail.com>
2019-01-07 17:10:36 -0700

    initial work on encrypt.rb

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -3,5 +3,13 @@ require './lib/shift'
 require './lib/offset'
 require './lib/key'
 
+handle = File.open(ARGV[0], "r")
+message = handle.read
+handle.close
+
 enigma = Enigma.new
-enigma
+encrypted = enigma.encrypt(message)
+
+writer = File.open(ARGV[1], "w")
+writer.write(encrypted)
+writer.close
