04163f28c6fbf1740799d8f569ac5e6ddbff64a1
earl-stephens <stephensearl@hotmail.com>
2019-01-07 21:38:39 -0700

    added Shift module, test_it_can_create_a_shift_pattern and create_shift_pattern methods

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,2 +1,10 @@
+# require './shift'
 class Encryption
+  include Shift
+  attr_reader :shift_pattern
+
+  def initialize
+    @shift_pattern = []
+  end
+
 end
--- test/encryption_test.rb
+++ test/encryption_test.rb
@@ -6,4 +6,17 @@ class EncryptionTest < Minitest::Test
 
     assert_instance_of Encryption, encryption
   end
+
+  def test_it_can_create_a_shift_pattern
+    # skip
+    encryption = Encryption.new
+    key = Key.new
+    offset = Offset.new
+    keys = key.main_test_method("02468")
+    offsets = offset.main_test_method("122212")
+
+    encryption.create_shift_pattern(keys, offsets)
+
+    assert_equal [4, 33, 50, 72], encryption.shift_pattern
+  end
 end
