583872b361efb4d2b136f300a0d8b79d57ec6c85
earl-stephens <stephensearl@hotmail.com>
2019-01-07 22:39:38 -0700

    added test_it_can_split_a_message_backwards and split_message_backwards and rotate_letter_backwards methods

--- lib/decryption.rb
+++ lib/decryption.rb
@@ -11,4 +11,21 @@ class Decryption
     @counter = 0
   end
 
+  def rotate_letter_backwards(character, counter)
+    placeholder = @alphabet.index(character)
+    backwards_shift = @shift_pattern[counter] * -1
+    rotated_alphabet = @alphabet.rotate(backwards_shift)
+    @shifted_message.concat(rotated_alphabet[placeholder])
+    @shifted_message
+  end
+  
+  def split_message_backward(character, counter)
+    if letter_in_alphabet?(character) == true
+      rotate_letter_backwards(character, counter)
+    else
+      @shifted_message.concat(character)
+    end
+    @shifted_message
+  end
+
 end
--- test/decryption_test.rb
+++ test/decryption_test.rb
@@ -13,4 +13,16 @@ class DecryptionTest < Minitest::Test
     assert_equal [], decryption.shift_pattern
   end
 
+  def test_it_can_split_a_message_backwards
+    decryption = Decryption.new
+    decryption.create_alphabet
+    key = Key.new
+    offset = Offset.new
+    keys = key.main_test_method("02715")
+    offsets = offset.main_test_method("040895")
+    decryption.create_shift_pattern(keys, offsets)
+
+    assert_equal "e", decryption.split_message_backward("h", 0)
+  end
+
 end
